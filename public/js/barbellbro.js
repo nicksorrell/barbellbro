b90f85dda5124dbba51d458db1f62f2701eab0fd
Nick Sorrell <nicksorrell@gmail.com>
2015-09-22 12:28:53 -0400

    initial commit

---
+++ public/js/barbellbro.js
@@ -0,0 +1,114 @@
+var barbellBro = {
+  settings: {
+    config: {
+        firstUse: 0,
+        activeWeightSet: 0,
+        activeWeight: 0,
+        debugMode: true
+    },
+
+    weightSets: [
+      {
+        name: "Regular",
+        type: "US",
+        weights: [ 45, 35, 25, 10, 5, 2.5, 1, 0.5 ],
+        weightStatus: [ 1, 1, 1, 1, 1, 1, 1, 1 ]
+      },
+      {
+        name: "Olympic",
+        type: "metric",
+        weights: [ 25, 20, 15, 10, 5, 2.5, 2, 1.5, 1, 0.5 ],
+        weightStatus: [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ]
+      },
+    ]
+  },
+
+  session: {
+    warmup: false
+  },
+
+  log: function(msg) {
+    if(this.settings.config.debugMode) {
+      console.log(msg);
+    }
+  },
+
+  setWeight: function(weight, warmup) {
+    //save current weight value
+    if(warmup != true) {
+      this.session.warmup = false;
+      this.log('Making working weight calculation...');
+      this.settings.config.activeWeight = weight;
+    } else {
+      this.session.warmup = true;
+      this.log('Making warmup calculation...');
+    }
+
+    //and calculate the plate load
+    return this.calcWeight(weight);
+  },
+
+  calcWeight: function(weight) {
+    //See what type of weights we're dealing with
+    var startWeight = (this.settings.weightSets[ this.settings.config.activeWeightSet ].type == "US") ? ( ( weight - 45 ) / 2 ) : ( ( weight - 20 ) / 2 );
+        weightSet = this.settings.weightSets[ this.settings.config.activeWeightSet ].weights,
+        originalWeight = startWeight,
+        numWeights = 0,
+        results = [];
+
+    if (startWeight < 0 ) {
+      startWeight = 0;
+    }
+
+    this.log("Using weight set: '" + this.settings.weightSets[ this.settings.config.activeWeightSet ].name + "' (" + this.settings.weightSets[ this.settings.config.activeWeightSet ].type + ")" );
+    this.log("Calculating weight for: " + weight + " (" + startWeight + " is our target with plates for each side.)");
+
+    //look at each plate in the list
+    for( var i = 0; i < weightSet.length; i++ ) {
+      if(this.settings.weightSets[ this.settings.config.activeWeightSet ].weightStatus[i] == 1) {
+        //if we can get a whole number of a certain plate, put that number on
+        if( Math.floor( startWeight / weightSet[i] ) > 0 ) {
+          this.log( weightSet[i] + ": " + Math.floor( startWeight / weightSet[i] ) );
+          results.push( Math.floor( startWeight / weightSet[i] ) );
+        } else {
+          this.log( weightSet[i] + ": " + Math.floor( startWeight / weightSet[i] ) );
+          results.push( 0 );
+        }
+        //remove that weight from the working total to figure out other plates
+        startWeight -= Math.floor( startWeight / weightSet[i] ) * weightSet[i];
+      } else {
+        results.push( 0 );
+      }
+    }
+    //return an array that matches the current active one in length
+    return { weight: weight, results: results }
+  },
+
+  getSetting: function(setting) {
+    return this.settings.config[String(setting)];
+  },
+
+  setSetting: function(setting, value) {
+    if(this.settings.config[setting] != null && value != null)
+      this.settings.config[setting] = String(value);
+  },
+
+  getWeightSet: function(setNum){
+    return this.settings.weightSets[Number(setNum)];
+  },
+
+  saveSettings: function() {
+    var settings = JSON.stringify(this.settings);
+    localStorage.setItem("barbellBroSettings", settings);
+    this.log("Saving settings... " + settings);
+  },
+
+  loadSettings: function() {
+    if(localStorage.getItem("barbellBroSettings") != null) {
+      this.settings = JSON.parse(localStorage.getItem("barbellBroSettings"));
+      this.log("Loading settings... " + this.settings);
+    } else {
+      this.log("No saved settings");
+    }
+  }
+};
---
+++ public/js/ui.js
@@ -0,0 +1,148 @@
+$(function(){
+  var activeWeightSet = barbellBro.getWeightSet( barbellBro.getSetting( 'activeWeightSet' ) ),
+      tempWeightSet = [],
+      weightStatus = 'on',
+      canvas = document.getElementById('grafix'),
+      ctx = canvas.getContext("2d"),
+      weightImgs = [],
+      imgObj, imgObj2,
+      drawCount = 0;
+
+  canvas.width = window.innerWidth;
+
+  function updateDisplay(weight, warmup){
+    //if(weight < 45) weight = 45;
+
+    var response = barbellBro.setWeight(weight, warmup),
+    theWeight = response.weight,
+    theResults = response.results,
+    weightTd,
+    weightTr;
+
+
+    /*************
+    * CANVAS STUFF (start)
+    **************/
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    grd = ctx.createLinearGradient(0,90,0,60);
+    grd.addColorStop(0,"#555");
+    grd.addColorStop(1,"#ccc");
+
+    /*
+    ctx.fillStyle = grd;
+    ctx.fillRect( 0, 60, 300, 30);
+    */
+
+    imgObj = new Image();
+    imgObj.src = 'img/bar.png';
+    imgObj.onload = function(){
+      ctx.drawImage( imgObj, 0, canvas.height/2 - 20, canvas.width * .95, 40 );
+    }
+
+    imgObj2 = new Image();
+    imgObj2.src = 'img/barstop.png',
+    imgObj2.onload = function(){
+      ctx.drawImage( imgObj2, 5, canvas.height / 2 - (80 / 2), 30, 80 );
+    }
+
+    drawCount = 0;
+    weightSize = 0;
+
+    for(var i = 0; i < theResults.length; i++ ) {
+      if(theResults[i] > 0) {
+        weightSize = activeWeightSet.weights[i];
+        for(var j = 0; j < theResults[i]; j++){
+          weightImgs[drawCount] = new Image();
+          weightImgs[drawCount].src = 'img/plate.png';
+          weightImgs[drawCount].onload = function(){
+            var _drawCount = drawCount,
+                _weightSize = weightSize;
+
+            return function(){
+              //ctx.drawImage( this, _drawCount*40, ((canvas.height/2)-(100 + _weightSize/2)), 35, 100 * (1 + _weightSize/100) );
+              ctx.drawImage( this, _drawCount*40 + ( i > 0 ? 35 : 0 ), canvas.height / 2 - ( 100 + canvas.height*( _weightSize/100 ) ) / 2 , 40, 100 + canvas.height * ( _weightSize/100 ) );
+              ctx.font = "20px Arial";
+              ctx.fillStyle = "#FFF";
+              ctx.textAlign = "center";
+              ctx.fillText( _weightSize, ( _drawCount * 40 ) + 20 + ( i > 0 ? 35 : 0 ), canvas.height / 2 + 5 );
+            };
+          }();
+
+          drawCount++;
+        }
+      }
+    }
+
+    /*************
+    * CANVAS STUFF (end)
+    **************/
+
+    $('input[name="weightInput"]').val( theWeight );
+    $('#weightTable').html("");
+
+    weightTr = $('<tr>');
+    for (var i = 0; i < theResults.length; i++){
+      weightTd = (theResults[i] == 0) ?
+        //TO DO: SEE ABOUT THE LINE BELOW ACTING WEIRD WITH THE GETTER
+        $('<td class="zero ' + (barbellBro.settings.weightSets[ barbellBro.settings.config.activeWeightSet ].weightStatus[ i ] == 1 ? '' : 'inactive') + '" data-num="' + i + '">') :
+        $('<td class="' + (activeWeightSet.weightStatus[ i ] == 1 ? '' : 'inactive') + '" data-num="' + i + '">');
+      weightTd.append( '<p class="heading">' + activeWeightSet.weights[i] + '</p><p>' + theResults[i] +'</p>');
+      weightTr.append( weightTd );
+    }
+
+    $('#weightTable').append(weightTr);
+  }
+
+  $('#bumpWeight').html( activeWeightSet.weights[ activeWeightSet.weights.length - 1 ] );
+
+  //DO I NEED TO MAKE THIS ADJUST IF THE LOWEST WEIGHT IS TOGGLED OFF?
+  $('a.btn-bump').first().attr('data-increment', activeWeightSet.weights[ activeWeightSet.weights.length - 1 ] * 2 ).html( "+" + activeWeightSet.weights[ activeWeightSet.weights.length - 1 ] * 2 );
+  $('a.btn-bump').last().attr('data-increment', -( activeWeightSet.weights[ activeWeightSet.weights.length - 1 ] * 2 ) ).html( -( activeWeightSet.weights[ activeWeightSet.weights.length - 1 ] * 2 ) );
+
+  $('a.btn-warmup').on('click', function(){
+    if( $(this).data( 'percent' ) == "100") {
+      updateDisplay( barbellBro.getSetting( 'activeWeight' ), false );
+    } else {
+      updateDisplay( barbellBro.getSetting( 'activeWeight' ) * ( $(this).data( 'percent' ) / 100 ), true );
+    }
+
+    $('a.btn-warmup').removeClass('btn-active');
+    $(this).addClass('btn-active');
+  });
+
+  $('a.btn-bump').on('click', function(){
+    var activeWeight = barbellBro.getSetting( 'activeWeight' );
+    updateDisplay( activeWeight += $(this).data( 'increment' ) );
+  });
+
+  $('input[name="weightInput"]').on('keyup', function(){
+    updateDisplay( Number( $(this).val() ) );
+  });
+
+  $('input').on('click', function(){
+    $(this).val("");
+  });
+
+  $('#weightTable').on('click', 'td', function(){
+    barbellBro.settings.weightSets[ barbellBro.settings.config.activeWeightSet ].weightStatus[ $(this).data('num') ] =
+      barbellBro.settings.weightSets[ barbellBro.settings.config.activeWeightSet ].weightStatus[ $(this).data('num') ] == 1 ? 0 : 1;
+
+    updateDisplay( Number( $('input[name="weightInput"]').val() ), barbellBro.session.warmup );
+  });
+
+  function init(){
+    barbellBro.loadSettings();
+    imgObj = new Image();
+    imgObj.src = 'img/bar.png';
+    imgObj.onload = function(){
+      ctx.drawImage( imgObj, 0, canvas.height/2 - 20, canvas.width*.9, 40 );
+    }
+
+    $('.tableBox .label').append( " (" + (barbellBro.settings.weightSets[ barbellBro.settings.config.activeWeightSet ].type == "US" ? "lb" : "kg") + ")" );
+  }
+
+  init();
+
+});
